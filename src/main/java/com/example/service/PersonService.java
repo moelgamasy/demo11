72f920ea1e105d6eef376d5054df938549a046a7
Mohamed ELgamasy <45573043+moelgamasy@users.noreply.github.com>
2020-12-19 15:59:45 +0200

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -15,7 +15,7 @@ public class PersonService {
 		Person p = new Person();
 		p.setId("1");
 		p.setAge(21);
-		p.setFirstName("ETISAALT");
+		p.setFirstName("cls");
 		p.setLastName("HOSAMJJJJ");
 		persons.add(p);
 
